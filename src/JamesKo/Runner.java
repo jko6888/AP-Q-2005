8fb5ae781145650977738e32987a91e131457430
jko6888 <43213100+jko6888@users.noreply.github.com>
2019-01-30 11:08:54 -0500

    Update Runner.java

--- src/JamesKo/Runner.java
+++ src/JamesKo/Runner.java
@@ -1,8 +1,20 @@
 package JamesKo;
 
 public class Runner {
-
     public static void main(String[] args) {
-        // write your code here
+        int[] scores0 = { 50, 50, 20, 80, 53 };
+        int[] scores1 = { 20, 50, 50, 53, 80 };
+        int[] scores2 = { 20, 50, 50, 80 };
+
+        StudentRecord sr0 = new StudentRecord(scores0);
+        StudentRecord sr1 = new StudentRecord(scores1);
+        StudentRecord sr2 = new StudentRecord(scores2);
+
+        StudentRecord[] sr = { sr0, sr1,  sr2 };
+
+        for (int i = 0; i < sr.length; i++) {
+            System.out.println("Score " + i + " improved? " + sr[i].improved());
+            System.out.println("Score " + i + " final average? " + sr[i].finalAverage());
+        }
     }
 }
